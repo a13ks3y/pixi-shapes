b31a2fae269f3ec5213a594a368a2c3ebca7efb4
a13k3y <a13ks3y.ass@gmail.com>
2026-04-03 14:21:37 +0300

    update REAMDE, add screenshot

--- README.md
+++ README.md
@@ -4,4 +4,11 @@
 ```bash
     npm run build
 ```
-
+## Deply (GitHub Pages)
+```bash
+    npm run deploy
+```
+## Demo
+[GitHub Pages](https://a13ks3y.github.io/pixi-shapes)
+## Screenshots
+![screenshot-1](./screenshots/screenshot-1.png)
---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Checkout gh-pages branch
+git checkout gh-pages
+
+# Build the project
+npm run build
+
+# Copy dist to docs
+cp -r dist/* docs/
+
+# Push changes
+git add docs/
+git commit -m "Update docs from dist"
+git push origin gh-pages
